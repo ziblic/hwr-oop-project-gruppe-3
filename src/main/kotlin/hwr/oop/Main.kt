e61d03fd4867efc9b61d7a8adb77d44f04d66ef1
Testspieler09 <pepehanisch06@gmail.com>
2025-04-08 21:13:59 +0200

    feat: add example argparser

--- src/main/kotlin/hwr/oop/Main.kt
+++ src/main/kotlin/hwr/oop/Main.kt
@@ -1,6 +1,32 @@
 package hwr.oop
 
-fun main() {
-    val answer = KotlinExample().sayHello()
-    println(answer)
+import com.xenomachina.argparser.ArgParser
+import com.xenomachina.argparser.default
+import com.xenomachina.argparser.mainBody
+
+class Arguments(parser: ArgParser) {
+    val verbose by parser.flagging("-v", "--verbose", help = "enable verbose mode")
+
+    val name by parser.storing("-N", "--name", help = "name of the widget").default("John Doe")
+
+    val size by parser.storing("-s", "--size", help = "size of the plumbus") { toInt() }
+
+    val sources by
+            parser.positionalList("SOURCE", help = "source filename", sizeRange = 1..Int.MAX_VALUE)
+
+    val destination by parser.positional("DEST", help = "destination filename")
+}
+
+fun main(args: Array<String>) = mainBody {
+    val parsedArgs = ArgParser(args).parseInto(::Arguments)
+    parsedArgs.run {
+        println(
+                """
+                verbose =     $verbose
+                name =        $name
+                size =        $size
+                sources =     $sources
+                destination = $destination""".trimIndent()
+        )
+    }
 }
