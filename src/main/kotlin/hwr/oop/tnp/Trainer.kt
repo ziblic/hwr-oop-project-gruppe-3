04b5493f12c18759f7016354b4b93b1e4a7a4691
Louis Böhlen <louisboehlen3112@gmail.com>
2025-04-16 11:54:32 +0200

    use constants for max monster const

--- src/main/kotlin/hwr/oop/tnp/Trainer.kt
+++ src/main/kotlin/hwr/oop/tnp/Trainer.kt
@@ -1,7 +1,6 @@
 package hwr.oop.tnp
 
 class Trainer(private val name: String, monsters: MutableList<Monster> = mutableListOf()) {
-    private val MAX_ALLOWED_MONSTERS_PER_TRAINER = 6
     private val monsters = if (monsters.size > MAX_ALLOWED_MONSTERS_PER_TRAINER)
         monsters.take(MAX_ALLOWED_MONSTERS_PER_TRAINER).toMutableList()
     else
