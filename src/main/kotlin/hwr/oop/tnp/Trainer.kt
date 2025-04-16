16fe19dd47bb486ab5492fb7adcfa47a342fbb1e
Louis Böhlen <louisboehlen3112@gmail.com>
2025-04-16 11:53:25 +0200

    feature: add `addMonster` to Trainer

--- src/main/kotlin/hwr/oop/tnp/Trainer.kt
+++ src/main/kotlin/hwr/oop/tnp/Trainer.kt
@@ -1,8 +1,9 @@
 package hwr.oop.tnp
 
-class Trainer(private val name: String, monsters: List<Monster> = emptyList()) {
+class Trainer(private val name: String, monsters: MutableList<Monster> = mutableListOf()) {
+    private val MAX_ALLOWED_MONSTERS_PER_TRAINER = 6
     private val monsters = if (monsters.size > MAX_ALLOWED_MONSTERS_PER_TRAINER)
-        monsters.take(MAX_ALLOWED_MONSTERS_PER_TRAINER)
+        monsters.take(MAX_ALLOWED_MONSTERS_PER_TRAINER).toMutableList()
     else
         monsters
 
@@ -12,5 +13,12 @@ class Trainer(private val name: String, monsters: List<Monster> = emptyList()) {
 
     fun getMonsters(): List<Monster> {
         return this.monsters
+    }
+
+    fun addMonster(monster: Monster): Boolean {
+        if (monsters.size >= MAX_ALLOWED_MONSTERS_PER_TRAINER) {
+            return false
+        }
+        return monsters.add(monster)
     }
 }
