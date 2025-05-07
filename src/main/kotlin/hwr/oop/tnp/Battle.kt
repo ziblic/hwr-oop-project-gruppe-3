90e34d314e75209c5e5bdb2c674445bc9db81985
Testspieler09 <pepehanisch06@gmail.com>
2025-05-07 16:24:36 +0200

    Revert " add Battle Class"
    This reverts commit 781ce3518858faf4c06abe49a21670ce3f10ef53.

--- src/main/kotlin/hwr/oop/tnp/Battle.kt
+++ src/main/kotlin/hwr/oop/tnp/Battle.kt
@@ -24,7 +24,14 @@ class Battle(
         finished = true
     }
 
-    fun executeTurn(attack1:Attack,attack2:Attack) {
+    /**
+     * 执行一回合的战斗：
+     *  - 先比较两只怪物的 speed，速度高的先手
+     *  - 先手攻击后如果被攻击方 KO 则直接结束战斗
+     *  - 否则再执行后手攻击，若再被 KO 也结束战斗
+     *  - 若双方都存活，则回合数加一
+     */
+    fun executeTurn() {
         if (finished) {
             throw IllegalStateException("Battle is already finished")
         }
@@ -34,40 +41,32 @@ class Battle(
         val m2 = trainerTwo.getMonsters().firstOrNull()
             ?: throw IllegalStateException("Trainer two has no monsters")
 
-        var first : Monster
-        var second : Monster
-        var firstAttack : Attack
-        var secondAttack :Attack
+        val (first, second) =
+            if (m1.getSpeed() >= m2.getSpeed()) m1 to m2 else m2 to m1
 
-            if (m1.getSpeed() >=m2.getSpeed()){
-                 first = m1
-                 second = m2
-                 firstAttack = attack1
-                 secondAttack = attack2
-                }else{
-                    first = m2
-                    second = m1
-                firstAttack = attack2
-                secondAttack = attack1
-
-            }
-
-
-       first.attack(firstAttack,second)
+        // 先手攻击
+        second.takeDamage(first.getAttack())
         if (second.isKO()) {
             endBattle()
             return
         }
 
-        second.attack(secondAttack, first)
+        // 后手攻击
+        first.takeDamage(second.getAttack())
         if (first.isKO()) {
             endBattle()
             return
         }
 
+        // 回合结束，双方都存活
         currentTurn++
     }
 
+    /**
+     * 返回获胜的 Trainer：
+     *  - 如果战斗没结束或平局，返回 null
+     *  - 否则返回存活的一方
+     */
     fun determineWinner(): Trainer? {
         if (!finished) return null
 
@@ -77,7 +76,7 @@ class Battle(
         return when {
             !m1.isKO() && m2.isKO() -> trainerOne
             m1.isKO() && !m2.isKO() -> trainerTwo
-            else -> null
+            else -> null  // 平局或异常情况
         }
     }
 }
